cbad5434b4adfa11f08d935b89dc89c2655d7807
arestus <arestus@gmail.com>
2021-12-22 19:05:18 +0200

    Installed d3 library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:'', component : LoginComponent},
-  {path:'home', component : HomeComponent}
+  { path: 'home', component: HomeComponent },
+  {path:'file-upload', component : FileUploadComponent}
 ];
 
 @NgModule({
